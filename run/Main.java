8623612b0b6fbcb7039fd5c1c6a433021a524eb8
Ștefan Neacșu-Miclea <91871146+stefnmUBB@users.noreply.github.com>
2022-10-10 21:49:19 +0300

    Add files via upload

---
+++ run/Main.java
@@ -0,0 +1,13 @@
+package run;
+
+
+import container.Strategy;
+import runner.StrategyTaskRunner;
+import sorter.MergeSorter;
+import sorter.SortStrategy;
+
+public class Main {
+    public static void main(String[] args) {
+        TestRunner.run();
+    }
+}
